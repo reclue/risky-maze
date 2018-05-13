a56947666776b8a39f22359e0361a69a0d8d8bd2
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-05-13 22:23:51 +0300

    Исключено понятие статичных стен

--- Assets/Script/Maze/Wall.cs
+++ Assets/Script/Maze/Wall.cs
@@ -8,9 +8,6 @@ namespace ru.lifanoff.Maze {
     /// </summary>
     public class Wall {
 
-        /// <summary>Если true, то значит, что эту стену нельзя убрать</summary>
-        public bool isStatic { get; private set; }
-
         /// <summary>Сторона расположения стены</summary>
         public Side side { get; private set; }
 
@@ -19,10 +16,8 @@ namespace ru.lifanoff.Maze {
 
 
         /// <param name="side">Сторона расположения стены</param>
-        /// <param name="isStatic">Можно ли убрать стену</param>
-        public Wall(Side side, bool isStatic = false) {
+        public Wall(Side side) {
             this.side = side;
-            this.isStatic = isStatic;
         }
 
     }//class
