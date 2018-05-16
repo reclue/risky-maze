7a72d65cd9c5216659e1abc4c899ea193a541bc8
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-05-16 22:30:25 +0300

    ...

---
+++ Assets/Script/Player/MiniMapCamera.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ru.lifanoff.Player {
+
+    /// <summary>
+    /// Скрипт управляющий камерой мини-карты
+    /// </summary>
+    [RequireComponent(typeof(Camera))]
+    public class MiniMapCamera : MonoBehaviour {
+
+        /// <summary>Текущая позиция камеры</summary>
+        private Vector3 currentCameraPosition = Vector3.zero;
+        /// <summary>Модуль расстояния от камеры до игрока по оси Y</summary>
+        private float posY = 5f;
+
+        /// <summary>Текущая позиция камеры</summary>
+        private Vector3 currentCameraRotation = Vector3.zero;
+        /// <summary>Постоянный поворот камеры по оси X</summary>
+        private float rotX = 90f;
+
+        /// <summary>Объект игрока</summary>
+        private GameObject currentPlayer;
+
+
+        #region Unity events
+        void Start() {
+            currentPlayer = SecondaryFunctions.GetPlayer();
+            currentCameraRotation.x = rotX;
+        }
+
+        void Update() {
+            UpdateCameraPosition();
+            UpdateCameraRotation();
+        }
+        #endregion
+
+        /// <summary>Обновить позицию камеры</summary>
+        private void UpdateCameraPosition() {
+            currentCameraPosition.x = currentPlayer.transform.position.x;
+            currentCameraPosition.y = currentPlayer.transform.position.x + posY;
+            currentCameraPosition.z = currentPlayer.transform.position.z;
+            transform.position = currentCameraPosition;
+        }
+
+        /// <summary>Обновить угол поворрота камеры</summary>
+        private void UpdateCameraRotation() {
+            currentCameraRotation.y = currentPlayer.transform.eulerAngles.y;
+            currentCameraRotation.z = currentPlayer.transform.eulerAngles.z;
+            transform.eulerAngles = currentCameraRotation;
+        }
+    }//class
+}//namespace
