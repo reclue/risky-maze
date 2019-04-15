f6514b6162d90066f8643fa8bd441af6f2f27e93
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-04-15 19:31:06 +0300

    audio controller

---
+++ Assets/Script/Util/VolumeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+namespace ru.lifanoff.Util {
+    /// <summary>
+    /// Класс, который контролирует звук
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class VolumeController : MonoBehaviour {
+
+        private AudioSource audioSource = null;
+
+        void Start() {
+            audioSource = GetComponent<AudioSource>();
+            StartCoroutine(CheckVolume());
+        }
+
+        private IEnumerator CheckVolume() {
+            while (true) {
+                if (PauseController.isPaused) {
+                    if (audioSource.isPlaying) {
+                        audioSource.Pause();
+                    }
+                } else {
+                    if (!audioSource.isPlaying) {
+                        audioSource.UnPause();
+                    }
+
+                    if (audioSource.volume != SaveManager.Instance.optionsManager.musicOptions.musicVolume) {
+                        audioSource.volume = SaveManager.Instance.optionsManager.musicOptions.musicVolume;
+                    }
+                }
+
+                yield return null;
+            }
+        }
+    }
+
+}
